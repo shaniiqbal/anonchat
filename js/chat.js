37158bbe07ab66b4f7e050239f64b1b9292de61c
Zeeshan iqbal <6973598+shaniiqbal@users.noreply.github.com>
2020-12-31 21:10:15 +0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,6 @@
 var currentUser,displayName,email,photoUrl,emailVerified,uid;
 
 var resetDB = function(){
-console.log(firebase.auth());
 /*    DeleteUsersResult result = await FirebaseAuth.DefaultInstance.DeleteUsersAsync();
     
     Console.WriteLine($"Successfully deleted {result.SuccessCount} users.");*/
@@ -20,45 +19,74 @@ var showdata = function(){
 			var you = {};
 			me.avatar = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
 			you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
-						
-			var allchat = firebase.firestore().collection("chat");
+			
+			var allAvatar = [];
+			var allchat = firebase.firestore().collection("chat").orderBy("createdAt");
 			allchat.onSnapshot(function(response) {
 				document.getElementById("mCSB_1_container").innerHTML = "";
 				response.docs.forEach(function(chat){
 					firebase.firestore().collection("users").doc(chat.data().userid).get()
 					.then(function(userdata) {
+							if(allAvatar.includes(chat.data().userid)){
+								allAvatar.push(chat.data().userid);
+							}
 							displayName = userdata.data().nickname;
-							console.log("Document successfully written!", userdata.data().createdAt);
-							avatararray = displayName.split(" ");	
+							//console.log("Document successfully written!", userdata.data().createdAt);
+							if(displayName){
+								avatararray = displayName.split(" ");	
+							}
+							
 							if(avatararray){
 								let counter = 0;
-								avatararray.forEach(function(item){									
+								avatararray.forEach(function(item){
+									
 									avatarText = item.charAt(0);
 									counter++;
 								});
 							}else{
 								avatarText = displayName.charAt(0);
 							}
-							
-							var date = new Date(chat.data().createdAt * 1000);
+						
+						/*	var date = new Date(chat.data().createdAt * 1000);
 							var hours = date.getHours();
 							var minutes = "0" + date.getMinutes();
 							var seconds = "0" + date.getSeconds();
 							if(	currentUser == chat.data().userid){
 								document.getElementById("mCSB_1_container").innerHTML += '<div class="message new">'
-								+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+chat.data().message+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
+								+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+
+								chat.data().message
+								+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
 								+'</div>';
 							}else{
 								document.getElementById("mCSB_1_container").innerHTML += '<div class="message message-personal new">'
-								+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+chat.data().message+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
+								+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+
+								chat.data().message
+								+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
 								+'</div>';
-							}		
+							}		*/
 						
 					})
 					.catch(function(error) {
 						console.error("Error writing document: ", error);
 					});
-								
+					
+					var date = new Date(chat.data().createdAt * 1000);
+					var hours = date.getHours();
+					var minutes = "0" + date.getMinutes();
+					var seconds = "0" + date.getSeconds();
+					if(	currentUser == chat.data().userid){
+						document.getElementById("mCSB_1_container").innerHTML += '<div class="message new">'
+						+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+
+						chat.data().message
+						+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
+						+'</div>';
+					}else{
+						document.getElementById("mCSB_1_container").innerHTML += '<div class="message message-personal new">'
+						+'<figure class="avatar"><i>'+avatarText+'</i></figure>'+
+						chat.data().message
+						+'<div class="timestamp">'+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)+'</div>'
+						+'</div>';
+					}					
 				})
 				
 				setTimeout(function(){
@@ -66,19 +94,12 @@ var showdata = function(){
 					var d = $('.messages .messages-content');
 					console.log(d.prop('scrollHeight'));
 					$('.messages .messages-content').animate({ scrollTop: d.prop('scrollHeight') }, 1000);
+					console.log("work");
 				},1000);
 				
 			});
 
-		  	firebase.firestore().collection("chat").onSnapshot(function(docs) {
-				docs.docs.forEach(function(item){
-					var data = item.data();
-					if(data.userid == currentUser.uid){
-						console.log("Current data: ", item.data());	
-					}
-					
-				});			
-			});
+		  
 		} else{
 			window.location = "login.html";
 		}
@@ -105,36 +126,6 @@ var insertchat = function(){
 	});
 		
 }
-// Get the input field
-var input = document.getElementById("message");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-	var message = document.getElementById("message").value;
-	if(!message){
-		alert("message is required");
-		return;
-	}
-	//console.log(message);
-	firebase.firestore().collection("chat").add({
-    message: message,
-	userid:currentUser,
-    createdAt: firebase.firestore.FieldValue.serverTimestamp()  
-	})
-	.then(function(docRef) {
-		console.log("Document successfully written!",docRef);
-		document.getElementById("message").value = "";
-	})
-	.catch(function(error) {
-		console.error("Error writing document: ", error);
-	});
-    // Trigger the button element with a click
-  }
-});
 
 
 var logout = function(){
